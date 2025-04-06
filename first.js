26252f4440402c9d066a694f11b5430bd2c20850
Khushi1841 <155899917+Khushi1841@users.noreply.github.com>
2025-04-06 15:44:43 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+// // Calculate the sum of numbers from 1 to 100
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+
+// Display the sum using an alert box
+alert("The sum of numbers from 1 to 100 is: " + sum);
+
+// Calculate the product of numbers from 1 to 50
+let product = 1;
+for (let i = 1; i <= 50; i++) {
+    product *= i;
+}
+
+// Display the product using an alert box
+alert("The product of numbers from 1 to 50 is: " + product);
